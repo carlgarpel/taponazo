46da633b34d416ce532c3cf39ca50ad15aa3b06e
carlgarpel <cgarcia@aytoalmeria.es>
2017-02-08 14:45:45 +0100

    ufff+

--- www/src/Howto.js
+++ www/src/Howto.js
@@ -5,11 +5,7 @@ Ball.Howto.prototype = {
 		this.buttonContinue = this.add.button(0, 0, 'screen-howtoplay', this.startGame, this);
 	},
 	startGame: function() {
-		if ('addEventListener' in document) {
-   			 document.addEventListener('deviceready', function() {
-       		 Ball.Game.acelerometro();
-   			 }, false);
-
+		
 		this.game.state.start('Game');
 	}
 };
