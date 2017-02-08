1b867659f32fa4e229811da3a274f716f719b19b
carlgarpel <cgarcia@aytoalmeria.es>
2017-02-08 09:46:28 +0100

    Game desde 0

---
+++ www/src/Game2.js
@@ -0,0 +1,15 @@
+//game-pruebas
+Ball.Game = function(game) {};
+
+Ball.Game.prototype = {
+	create: function() {
+
+	},
+	update: function() {
+		
+	},
+render: function() {
+		// this.game.debug.body(this.ball);
+		// this.game.debug.body(this.hole);
+	}
+};
